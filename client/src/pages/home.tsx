5b5c250f2f4918e02f0a98e11306d2f14f638cb1
ankit512kumar <40004726-ankit512kumar@users.noreply.replit.com>
2025-02-26 02:12:51 +0000

    Update Home page with revised professional experience description.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/d2c1296d-66a9-4ebe-a07f-5549bdea42c8/e7e50f2c-ba67-4706-a4fd-43646960816e.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -43,11 +43,11 @@ export default function Home() {
               transition={{ delay: 0.6, duration: 0.6 }}
               className="body-lg max-w-2xl"
             >
-              I am currently serving as a Volunteer Product Manager for AI at Oakyard.
+              Currently serving as a Volunteer Product Manager for AI at Oakyard.
+              <br />
               <br />
               Former Product Owner at Losung360 and Product Engineering Specialist at EY.
               <br />
-              From my master's in management, I built my technical expertise in ‍generative AI as well as data strategy.
               <br />
               With an entrepreneurial spirit and analytical mindset, I've ‍built impactful products utilizing machine learning, generative AI, and data engineering solutions.
             </motion.p>
